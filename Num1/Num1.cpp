f3eb1d28b193ddd6ef3d53bb12b999baacaf4031
TopCoderDanil <61113532+TopCoderDanil@users.noreply.github.com>
2020-04-15 18:32:19 +0300

    Add files via upload

---
+++ Num1/Num1.cpp
@@ -0,0 +1,88 @@
+﻿
+#include <iostream>
+using namespace std;
+template<typename T>
+class Turn
+{
+public:
+    int n = 0;
+    T* value;
+    int N;
+    Turn(int N)
+    {
+        value = new T[N];
+        this->N = N;
+    }
+    /*Функция добовления элемента*/
+    void push(T importance)
+    {
+        if (n == N)
+        {
+            cout << "Переполнение очереди" << endl;
+        }
+        else
+        {
+            value[n] = importance;
+            n++;
+        }
+    }
+    /*Функция удаления элемента*/
+    void pop()
+    {
+        for (int i = 0; i < n; i++)
+        {
+            value[i] = value[i + 1];
+        }
+        n--;
+    }
+    /*Функция вывода очереди*/
+    void Show()
+    {
+        for (int i = 0; i < n; i++)
+        {
+            cout << value[i] << " ";
+        }
+        cout << endl;
+    }
+    /*Функция объеденения очередей*/
+    void Union(Turn op)
+    {
+        T* arr = new T[N];
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = value[i];
+        }
+        int y = n;
+        n = n + op.n;
+        N = N + op.N;
+        value = new T[N];
+        int j = 0;
+        for (int i = y; i < n; i++)
+        {
+            value[i] = op.value[j];
+            j++;
+        }
+        for (int i = 0; i < y; i++)
+        {
+            value[i] = arr[i];
+        }
+    }
+};
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    Turn<int> Ob(4);
+    Ob.push(10);
+    Ob.push(20);
+    Ob.push(30);
+    Ob.push(40);
+    Ob.Show();
+    Ob.pop();
+    Ob.Show();
+    Turn<int> Bob(4);
+    Bob.push(300);
+    Bob.push(400);
+    Bob.Show();
+    Bob.Union(Ob);
+    Bob.Show();
+}
---
+++ Num2/Num2.cpp
@@ -0,0 +1,181 @@
+﻿
+#include <iostream>
+using namespace std;
+
+template<typename T>
+class Turn
+{
+public:
+    Turn()
+    {
+        Push(1);
+    }
+    Turn(T n)
+    {
+        Push(n);
+    } 
+    /*Функция внесения элемента в бинарное дерево*/
+    void Push(T n)
+    {
+        pushPr(n, &tree);
+    }
+    /*Функция вывода элементов бинарного дерева*/
+    void Print()
+    {
+        printPr(tree, 0);
+    }
+    /*Функция удаления элемента из бинарного дерева*/
+    void Delete_element(T n)
+    {
+        DeleteNode(tree, n);
+    }
+    /*Функция просмотра листьев бинарного дерева*/
+    void Leaf_output()
+    {
+        printLeaf(tree);
+    }
+    /*Функция поиска элемента в бинарном дереве*/
+    void Element_search(T n)
+    {
+        if (find(tree, n))
+            cout << "Элемент найден" << endl;
+        else
+        {
+            cout << "Элемент не найден" << endl;
+        }
+        
+    }
+private:
+
+    struct node
+    {
+        T info;
+        node* l, * r;
+    };
+
+    node* tree = NULL;
+
+    void pushPr(T a, node** t)
+    {
+        if ((*t) == NULL)                  
+        {
+            (*t) = new node;               
+            (*t)->info = a;               
+            (*t)->l = (*t)->r = NULL;      
+            return;                        
+        }   
+        if (a > (*t)->info) pushPr(a, &(*t)->r);
+        else pushPr(a, &(*t)->l);        
+    }
+    void printPr(node* t, int u)
+    {
+        if (t == NULL) return;                
+        else
+        {
+            printPr(t->l, ++u);                   
+            for (int i = 0; i < u; ++i) cout << "|";
+            cout << t->info << endl;           
+            u--;
+        }
+        printPr(t->r, ++u);                      
+    }
+    node* DeleteNode(node* value,T val) {
+        if (value == NULL)
+            return value;
+
+        if (val == value->info) {
+
+            node* tmp;
+            if (value->r == NULL)
+                tmp = value->l;
+            else {
+
+                node* ptr = value->r;
+                if (ptr->l == NULL) {
+                    ptr->l = value->l;
+                    tmp = ptr;
+                }
+                else {
+
+                    node* pmin = ptr->l;
+                    while (pmin->l != NULL) {
+                        ptr = pmin;
+                        pmin = ptr->l;
+                    }
+                    ptr->l = pmin->r;
+                    pmin->l = value->l;
+                    pmin->r = value->r;
+                    tmp = pmin;
+                }
+            }
+
+            delete value;
+            return tmp;
+        }
+        else if (val < value->info)
+            value->l = DeleteNode(value->l, val);
+        else
+            value->r = DeleteNode(value->r, val);
+        return value;
+    }
+    void printLeaf(node* t)
+    {
+        if ((t->l == nullptr) && (t->r == nullptr))
+            cout << t->info << "\n";
+        else
+        {
+            if (t->l) printLeaf(t->l);
+            if (t->r) printLeaf(t->r);
+        }
+    }
+    bool find(node* r, T d)
+    {
+        if (r == NULL)
+        {
+            return false;  
+        }
+        if (r->info == d)
+        {
+            return true; 
+        }
+        if (d <= r->info)
+        {
+            if (r->l != NULL)
+                return find(r->l, d); 
+            else
+            {
+                return false; 
+            }
+        }
+        else
+        {
+            if (r->r)
+                return find(r->r, d);
+            else
+            {
+                return false; 
+            }
+        }
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    Turn<int> ob(10);
+    ob.Push(100);
+    ob.Push(70);
+    ob.Push(60);
+    ob.Push(77);
+    ob.Push(44);
+    ob.Push(1);
+    ob.Print();
+    ob.Delete_element(60);
+    cout << endl;
+    ob.Print();
+    cout << endl;
+    ob.Leaf_output();
+    cout << endl;
+    ob.Element_search(44);
+    ob.Element_search(1000);
+}
